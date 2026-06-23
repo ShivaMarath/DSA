d3319936e8fb402d37c58f11c860e25416dfcae4
Shiva Marath <shivamarath2005@gmail.com>
2026-06-23 05:36:10 +0530

    Time: 27 ms (22.81%) | Memory: 17.4 MB (54.29%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin() , nums.end());
-        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
         int n = nums.size();
-        for(int i = 0; i<n-3; i++){
+        for (int i = 0; i < n - 3; i++) {
             if (i > 0 && nums[i] == nums[i-1]) continue;
-            for(int j = i+1; j<n-2; j++){
-                if (j > i+1 && nums[j] == nums[j-1]) continue;
-                int k = j+1; 
-                int l = n-1;
-                while(k<l){
-                     long long sum = (long long) nums[i]+nums[j]+nums[k]+nums[l];
-                     if(sum == target){
-                        ans.push_back({nums[i] , nums[j] , nums[k] , nums[l]});
-                       
-                        while (k < l && nums[k] == nums[k+1]) k++; 
-                        while (k < l && nums[l] == nums[l-1]) l--;
-                         k++;
-                         l--;
-                     }
-                     else if(sum<target){
-                        k++;
-                     }
-                     else if(sum>target){
-                        l--;
-                     }
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j-1]) continue;
+                int l = j + 1, r = n - 1;
+                while (l < r) {
+                    long sum = (long)nums[i] + nums[j] + nums[l] + nums[r];
+                    if (sum == target) {
+                        res.push_back({nums[i], nums[j], nums[l], nums[r]});
+                        while (l < r && nums[l] == nums[l+1]) l++;
+                        while (l < r && nums[r] == nums[r-1]) r--;
+                        l++; r--;
+                    } else if (sum < target) l++;
+                    else r--;
                 }
             }
         }
-        return ans;
+        return res;
     }
 };
